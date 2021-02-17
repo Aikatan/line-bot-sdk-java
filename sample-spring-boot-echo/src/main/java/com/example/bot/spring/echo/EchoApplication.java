2e07aea3844d93ee4fa88d6d64be9ed188c842f4
Aikatan <AikaAikatan@users.noreply.github.com>
2021-02-17 16:39:16 +0700

    Update EchoApplication.java

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -42,6 +42,7 @@ public class EchoApplication {
     public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         log.info("event: " + event);
         final String originalMessageText = event.getMessage().getText();
+		String originalMessageText = "123456";
         return new TextMessage(originalMessageText);
     }
 
